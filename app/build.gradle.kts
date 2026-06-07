8fbbe22f01fcacbf284a9676b982f3d31e9ca7f8
Godfred Fokuo <133538964+EasWay@users.noreply.github.com>
2026-06-07 13:41:10 +0000

    Bump version to 2.0 (versionCode 10) for Google Play
    Current active release on Play Store is versionCode 9 / versionName 1.9.
    Next release must be versionCode 10 / versionName 2.0.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 10
-        versionName = "1.8"
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
